7a28bc3d35a701293bd5a781e8b010ac1a66874d
Geri <stanilova.gergana@gmail.com>
2023-04-21 17:28:05 +0200

    implantmotif doesnt work

--- implant_motif/src/implantmotif.cpp
+++ implant_motif/src/implantmotif.cpp
@@ -19,6 +19,7 @@
 #include <fstream>
 #include <vector>
 #include <sstream>
+#include <map>
 
 using namespace std;
 using namespace seqan;
@@ -112,23 +113,43 @@ std::string generateRandomSequence(int n) {
     }
     return dnaString;
 }
+std::string getConsensusSequence(const std::vector<std::string>& motifs) {
+    // Determine length of motifs
+    const int motifLength = motifs[0].length();
 
-bool freePos(int& i, vector<int>& p) {
-    for(int a : p)
-        if(i == a) 
-            return false;
-    return true;
-}
+    // Create a matrix to hold the counts of each nucleotide at each position
+    std::vector<std::vector<int>> counts(motifLength, std::vector<int>(4, 0)); // 4 = number of nucleotides
 
-// from https://www.geeksforgeeks.org/hamming-distance-two-strings/
-int hammingDist(DnaString str1, DnaString str2) {
-    int i = 0, count = 0;
-    for(int x = 0; x < length(str1); x++) {
-        if (str1[i] != str2[i])
-            count++;
-        i++;
+    // Fill the counts matrix
+    for (const auto& motif : motifs) {
+        for (int i = 0; i < motifLength; i++) {
+            switch (motif[i]) {
+                case 'A': counts[i][0]++; break;
+                case 'C': counts[i][1]++; break;
+                case 'G': counts[i][2]++; break;
+                case 'T': counts[i][3]++; break;
+                default: break;
+            }
+        }
     }
-    return count;
+
+    // Determine the most frequent nucleotide at each position
+    std::string consensus(motifLength, 'N'); // 'N' = undefined nucleotide
+    for (int i = 0; i < motifLength; i++) {
+        const int maxCount = *std::max_element(counts[i].begin(), counts[i].end());
+        if (maxCount > 0) {
+            const int index = std::distance(counts[i].begin(), std::find(counts[i].begin(), counts[i].end(), maxCount));
+            switch (index) {
+                case 0: consensus[i] = 'A'; break;
+                case 1: consensus[i] = 'C'; break;
+                case 2: consensus[i] = 'G'; break;
+                case 3: consensus[i] = 'T'; break;
+                default: break;
+            }
+        }
+    }
+
+    return consensus;
 }
 
 int main(int argc, char const ** argv)
@@ -146,34 +167,59 @@ int main(int argc, char const ** argv)
             {26, 19, 0, 84, 2, 28, 2, 5, 2, 13, 13},
             {19, 11, 90, 5, 1, 0, 87, 0, 3, 51, 30}
     };
+    int motif_length = gcn4_jaspar_matrix[0].size();
 
     vector<vector<double>> normalized_gcn4_jaspar_matrix = normalizeMatrix(gcn4_jaspar_matrix);
 
     printMatrix(normalized_gcn4_jaspar_matrix);
 
-    // Generate 20 randomized DNA strings based on the input matrix
+
+    // Generate motifs
+    vector<string> motifs;
     for (int i = 0; i < t; i++) {
-        std::string randomString = generateRandomMotifFromMatrix(normalized_gcn4_jaspar_matrix);
-        std::cout << randomString << std::endl;
+        motifs.push_back(generateRandomMotifFromMatrix(normalized_gcn4_jaspar_matrix));
     }
 
-
-    string filename = "implanted_gcn4.fasta"; // Output filename
-
-    ofstream outfile(filename);
-    if (!outfile) {
-        cerr << "Error: could not open file \"" << filename << "\" for writing.\n";
-        return 1;
-    }
-
+    // Generate random sequences
+    vector<string> sequences;
     for (int i = 0; i < t; i++) {
-        string seq = generateRandomSequence(n);
-        outfile << ">seq_" << i << "\n";
-        outfile << seq << "\n";
+        sequences.push_back(generateRandomSequence(n));
     }
 
-    outfile.close();
-    cout << "Generated " << t << " random sequences of length " << n << " and saved them to file \"" << filename << "\".\n";
+    // Implant motifs in each sequence and record positions and motifs
+    vector<tuple<int, int, string>> motif_pos;
+    for (int i = 0; i < t; i++) {
+        int pos = rand() % (n - 20); // choose a random position to implant the motif
+
+        for (int i = 0; i < motifs.size(); i++) {
+            string motif = motifs[i];
+            sequences[i].replace(pos, 20, motif); // implant motif
+            motif_pos.push_back(make_tuple(i, pos, motif)); // record position and motif
+        }
+
+    }
+
+    // Write sequences to FASTA file
+    ofstream fastaFile("gcn4_sequences.fasta");
+    for (int i = 0; i < t; i++) {
+        fastaFile << ">seq_" << i << endl;
+        fastaFile << sequences[i] << endl;
+    }
+    fastaFile.close();
+
+    string csvFilePath = "motifs.csv";
+    string consensus = getConsensusSequence(motifs);
+
+    // Write motifs to CSV file
+    ofstream csvFile("implanted_motifs.csv");
+    csvFile << "seq\tpos\tmotif" << endl;
+    csvFile << "X\tX\t" << consensus << endl; // write consensus sequence
+    for (auto m : motif_pos) {
+        csvFile << get<0>(m) << "\t" << get<1>(m) << "\t" << get<2>(m) << endl;
+    }
+    csvFile.close();
+
+
 
 
 
